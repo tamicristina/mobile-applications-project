bfccc5546c46b64b3c03a4e3863b7e5e34e111fa
tamicristina <tami.cristina06@gmail.com>
2025-06-03 21:57:00 -0300

    removing unused import

--- App.tsx
+++ App.tsx
@@ -1,5 +1,4 @@
 import { NavigationContainer } from "@react-navigation/native";
-import { StyleSheet } from "react-native";
 import AppRoutes from "./src/routes/app.routes";
 
 export default function App() {
